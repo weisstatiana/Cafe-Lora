abb4a6752b6c2ba4e88f3198aa67af753e9f8b36
Tatiana Ondzikova <tatianaondzikova4@gmail.com>
2021-05-01 12:06:01 +0200

    ukazovanie a skrývanie navigácie

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import './style.css';
 
 console.log('funguju!');
+
+const navElm = document.querySelector('nav');
+const navBtn = document.querySelector('#nav-btn');
+const showHideNaw = () => {
+  navElm.classList.toggle('nav-closed');
+};
+navBtn.addEventListener('click', showHideNaw);
+
+navElm.forEach((elm) => {
+  elm.addEventListener('click', showHideNaw);
+});
