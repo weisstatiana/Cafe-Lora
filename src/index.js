8e579eae63291e9ffef70bf7297e7a7b1b3a3394
Tatiana Ondzikova <tatianaondzikova4@gmail.com>
2021-05-01 12:40:35 +0200

    objednávaní

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './style.css';
 
 console.log('funguju!');
+/* sprovoznenie navigacie*/
 
 const navElm = document.querySelector('nav');
 const navBtn = document.querySelector('#nav-btn');
@@ -9,6 +10,23 @@ const showHideNaw = () => {
 };
 navBtn.addEventListener('click', showHideNaw);
 
-navElm.forEach((elm) => {
+const navItems = document.querySelectorAll('nav a');
+navItems.forEach((element) => {});
+(elm) => {
   elm.addEventListener('click', showHideNaw);
+};
+
+/* objednávanie*/
+let ordered = false;
+const orderBtn = document.querySelector('.order-btn');
+orderBtn.addEventListener('click', () => {
+  if (ordered === false) {
+    document.querySelector('.drink__cup').classList.add('drink__cup--selected');
+    ordered = true;
+    orderBtn.textContent = 'zrušiť';
+  } else {
+    document.querySelector('.drink__cup').classList.remove;
+    orderBtn.textContent = 'objednať';
+    ordered = false;
+  }
 });
